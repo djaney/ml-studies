3031aca883fb5402ce7a85bc530020e11480598e
Djane Rey Mabelin <d.mabelin@arcanys.com>
2017-12-15 09:51:00 +0800

    kmeans

--- 02_kmeans.py
+++ 02_kmeans.py
@@ -1,9 +1,44 @@
+import numpy as np
 import tensorflow as tf
-points = tf.Variable(tf.random_normal([2, 100]))
-centroids = tf.Variable(tf.random_normal([2, 3]))
+from tensorflow.contrib.factorization import KMeans
 
-model = tf.global_variables_initializer()
+num_features = 2
+clusters = 5
+
+data = tf.random_normal([3, num_features])
+
+x = tf.placeholder(tf.float32, shape=[None, num_features])
 
 
-with tf.Session() as session:
-    print(session.run(model))
+kmeans = KMeans(inputs=x, num_clusters=clusters, distance_metric='cosine',
+use_mini_batch=True)
+
+# Build KMeans graph
+training_graph = kmeans.training_graph()
+
+(all_scores, cluster_idx, scores, cluster_centers_initialized, cluster_centers_var, init_op, train_op) = training_graph
+
+cluster_idx = cluster_idx[0] # fix for cluster_idx being a tuple
+
+avg_distance = tf.reduce_mean(scores)
+
+# Initialize the variables (i.e. assign their default value)
+init_vars = tf.global_variables_initializer()
+
+# Start TensorFlow session
+sess = tf.Session()
+
+array_data = sess.run(data)
+
+sess.run(init_vars, feed_dict={x: array_data})
+sess.run(init_op, feed_dict={x: array_data})
+
+# Iterate kmeans algorithm
+for i in range(100):
+    _, d, data_clusters_idx = sess.run([train_op, avg_distance, cluster_idx], feed_dict={x: array_data})
+
+groupings = [[] for x in range(clusters)]
+for i in range(len(data_clusters_idx)):
+	groupings[data_clusters_idx[i]].append(array_data[i])
+
+print(groupings)
