57d2258bcceeca246af6a5402ea857b5fe5583d1
Djane Rey Mabelin <d.mabelin@arcanys.com>
2017-12-18 13:12:26 +0800

    simple nn

--- 03_color_temp.py
+++ 03_color_temp.py
@@ -1,3 +1,9 @@
+
+'''
+Very simple neural network that identifies if the three numbers are high or low.
+labels has 2 values, for low (0) and high (1)
+'''
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -6,67 +12,52 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
-tf.logging.set_verbosity(tf.logging.INFO)
 
-
+# just 2 test datas
 data = np.array([
     [1.0, 1.0, 1.0],
     [0.0, 0.0, 0.0]
 ])
 
-labels = np.array([[0,1],[1,0]])
-
-batch_size = len(data);
-num_steps = 1000
-num_features = 3;
-num_classes = 2;
-learning_rate = 0.3
-x = tf.placeholder(tf.float32, shape=[batch_size,num_features])
-y = tf.placeholder(tf.int32, shape=[batch_size,num_classes])
-
-def cnn_model_fn(features, labels, mode):
-    hidden_layer = tf.layers.dense(inputs=features['colors'], units=16, activation=tf.nn.relu)
-    logits = tf.layers.dense(inputs=hidden_layer, units=num_classes, activation=tf.nn.relu) # 2 classifications
-    # Predictions
-    pred_classes = tf.argmax(logits, axis=1)
-    pred_probas = tf.nn.softmax(logits)
-
-    # If prediction mode, early return
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode, predictions=pred_classes)
-
-    loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits( logits=logits, labels=tf.cast(labels, dtype=tf.int32)))
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-    train_op = optimizer.minimize(loss_op, global_step=tf.train.get_global_step())
-
-    # Evaluate the accuracy of the model
-    acc_op = tf.metrics.accuracy(labels=labels, predictions=pred_classes)
-
-    # TF Estimators requires to return a EstimatorSpec, that specify
-    # the different ops for training, evaluating, ...
-    return tf.estimator.EstimatorSpec(
-        mode=mode,
-        predictions=pred_classes,
-        loss=loss_op,
-        train_op=train_op,
-        eval_metric_ops={'accuracy': acc_op})
-
-model = tf.estimator.Estimator(cnn_model_fn)
-
-input_fn = tf.estimator.inputs.numpy_input_fn( x={'colors': data}, y=labels,
-batch_size=batch_size, num_epochs=None, shuffle=True)
-
-# Train the Model
-model.train(input_fn, steps=num_steps)
+# labels or the correct answers of the test datas
+labels = np.array([[0.0,1.0],[1.0,0.0]])
 
 
-# Evaluate the Model
-# Define the input function for evaluating
-input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={'colors': data}, y=lables,
-batch_size=batch_size, shuffle=False)
+# input - None is for batch, 3 is for number of input per batch
+x = tf.placeholder(tf.float32, [None,3])
+w = tf.Variable(tf.zeros([3,2])) # create 3 weights (1 for each input) for 2 neurons
+b = tf.Variable(tf.zeros([2])) # create a bias for each neuron. This is just another weight that we do not matmul with the input
 
-# Use the Estimator 'evaluate' method
-e = model.evaluate(input_fn)
+# initialize the variables defined above
+init = tf.initialize_all_variables()
 
-print("Testing Accuracy:", e['accuracy'])
+# model using softmax activation function. Formula is always input * weights + bias then feed to the activation
+m = tf.nn.softmax(tf.matmul(x, w) + b)
+
+# labes or correct answers
+y = tf.placeholder(tf.float32, [None, 2])
+
+# calculate the loss distance using cross entropy
+cross_entropy = -tf.reduce_sum(y * tf.log(m))
+
+# check if highest probability is the same as correct answers
+is_correct = tf.equal(tf.argmax(m,1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+
+optimizer = tf.train.GradientDescentOptimizer(0.003)
+train_step = optimizer.minimize(cross_entropy)
+
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+	train_data = {x: data, y: labels}
+	a,c = sess.run([accuracy, cross_entropy], feed_dict=train_data)
+	sess.run(train_step, feed_dict=train_data)
+
+	if (0 == i % 100):
+		a,c = sess.run([accuracy, cross_entropy], feed_dict=train_data)
+		print(a,c)
+
+r = sess.run(m, feed_dict=train_data)
+print(r)
