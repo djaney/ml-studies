3d1621bd5c04c1a54bac1c017f478785b4f876e1
Djane Rey Mabelin <thedjaney@gmail.com>
2018-03-05 00:49:42 +0800

    updates

---
+++ translator.py
@@ -0,0 +1,35 @@
+import sys
+import glob
+from classes.base import Seq2Seq
+from keras.preprocessing.text import text_to_word_sequence
+
+
+class BisayaSeqToSeq(Seq2Seq):
+	def load_data(self):
+		data = []
+		with open('data/translate/beki-bisaya.txt') as file:
+			for line in list(file):
+				arr = line.split('|')
+				data.append((text_to_word_sequence(arr[0].strip()), text_to_word_sequence(arr[1].strip())))
+
+		return data
+
+def main():
+
+
+	if 'train' == sys.argv[1]:
+		agent = BisayaSeqToSeq(epochs=100)
+		model = agent.train()
+		model.save('.models/chatbot.h5')
+	elif 'play' == sys.argv[1]:
+		agent = BisayaSeqToSeq(model_filename='.models/chatbot.h5')
+
+
+	while True:
+		out, unk = agent.evaluate(text_to_word_sequence(input('Input: ')))
+		if 0 == len(unk):
+			print('Output: '+' '.join(out))
+		else:
+			print('unable to translate')
+
+main()
